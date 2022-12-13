a7cbdbb2bdc6f411282524d16dfb6a57e57c6eff
Hovsep Mkrtchyan <hovsepmkrtchyan15@gmail.com>
2022-12-14 01:23:17 +0400

    admin functionally, login, register

--- src/main/java/com/example/common/repository/RegionRepository.java
+++ src/main/java/com/example/common/repository/RegionRepository.java
@@ -4,10 +4,12 @@ import com.example.common.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RegionRepository extends JpaRepository<Region,Integer> {
 
     List<Region> findByNameContaining(String keyword);
 
+    Optional<Region> findByName(String name);
 
 }
